ea9070bf920c7e0391e0542269e4711774f2f9a2
RebeccaRosies <rebecca.rosies@student.ehb.be>
2021-11-30 17:37:57 +0100

    working get challenges route

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
-const express = require ('express');
-const bodyParser = require ('body-parser');
+const credentials = require("./credentials.js");
+const {
+    MongoClient
+} = require("mongodb")
+const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 const port = process.env.PORT || 1337;
+
+// extra variable & info fr mongodb to work
+const url = `mongodb+srv://teamwork:britt@cluster0.fkenc.mongodb.net/teamwork?retryWrites=true&w=majority`;
+const client = new MongoClient(url);
+console.log(credentials)
+
+// The database to use
+
+const dbName = "teamwork";
 
 app.use(express.static('public'))
 app.use(bodyParser.json());
@@ -12,17 +25,41 @@ app.get('/', (req, res) => {
     res.send('Everything is OK!')
 })
 
-app.get('/challenges', (req, res) => {
-    let data = {
-        name: "Challenge1",
-        course: "webII",
-        points: 1000
+app.get('/challenges', async (req, res) => {
+    //get data from mongo en send naar res
+    // Demo op github (ex3)
+    //this is the same but data on small level without mongodb
+    /*    let data = {
+           name: "Challenge1",
+           course: "webII",
+           points: 1000
+       }
+       res.send(data) */
+    try {
+        //connect to the database
+        await client.connect();
+        console.log("Connected correctly to server");
+
+        const db = client.db(dbName);
+        // Use the collection "people"
+        const col = db.collection("challenges");
+        // Find document
+        const myDoc = await col.find({}).toArray();
+
+        // Print to the console
+        console.log(myDoc);
+        //Send back the data with the response
+        res.status(200).send(myDoc);
+    } catch (err) {
+        console.log(err.stack);
+    } finally {
+        await client.close();
     }
-    res.send(data)
 })
 
+
 // create server with 'port' as fisrt variable & callback function as the second variable
-app.listen(port,() => {
+app.listen(port, () => {
     console.log(`API is running at http://localhost:${port}`);
-} )
+})
 console.log("test")
