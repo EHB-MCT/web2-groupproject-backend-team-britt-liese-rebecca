5b0595deadf45428e6cd5b2c146ba71251b1dfe7
RebeccaRosies <rebecca.rosies@student.ehb.be>
2021-11-30 16:18:58 +0100

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
+const express = require ('express');
+const app = express();
+const port = 1337;
+
+app.use(express.static('public'))
+
+
+//get route -> every route has at least 2 parameters at least: the url & the calback function with request & response
+app.get('/', (req, res) => {
+    res.send('Everything is OK!')
+})
+
+// create server with 'port' as fisrt variable & callback function as the second variable
+app.listen(port,() => {
+    console.log(`API is running at http://localhost:${port}`);
+} )
 console.log("test")
