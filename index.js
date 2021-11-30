4c5267d328ee5cf5421874bab2013e0f2c18cee4
RebeccaRosies <rebecca.rosies@student.ehb.be>
2021-11-30 16:32:17 +0100

    New route challenges

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
 const express = require ('express');
+const bodyParser = require ('body-parser');
 const app = express();
 const port = process.env.PORT || 1337;
 
 app.use(express.static('public'))
+app.use(bodyParser.json());
 
 
 //get route -> every route has at least 2 parameters at least: the url & the calback function with request & response
 app.get('/', (req, res) => {
     res.send('Everything is OK!')
+})
+
+app.get('/challenges', (req, res) => {
+    let data = {
+        name: "Challenge1",
+        course: "webII",
+        points: 1000
+    }
+    res.send(data)
 })
 
 // create server with 'port' as fisrt variable & callback function as the second variable
